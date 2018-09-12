bfe134a7abf7a3aa4c536d2d81af8a0a167c43e6
christine gomez <christine.gomez1226@gmail.com>
2018-09-12 20:26:27 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,11 +5,10 @@ def reformat_languages(languages)
       if new_hash[name] ||= attributes
         new_hash[name][:style] = []
         new_hash[name][:style] << style
-      else new_hash[name] = {[:type] attributes[:type],
-      [:style]style}
+      else new_hash[name] = {type: attributes[:type],
+      styles: [style]}
     end
   end
-end
 end
   new_hash
 end
